a932a7e7556e5e82e75b6584793b0cd1a5232c24
Christian Langridge <chris.p.langridge@gmail.com>
2025-06-11 23:33:08 +0100

    Update Google course exercise 1.py

--- Google course exercise 1.py
+++ Google course exercise 1.py
@@ -1,18 +1,1 @@
-import sys
-def main():
-    print("Hello there", sys.argv[1])
-if __name__ == "__main__":
-    main()
-
-# Defines a "repeat" function that takes 2 arguments
-
-def repeat(s, exclaim):
-    "Returns a string repeated 3 times, with an optional exclamation mark"
-    "If exclaim is true, add exclamation marks"
-
-    result = s * 3
-    if exclaim:
-        result = result + '!!!'
-    return result
-
-repeat("crackers")
+print("fish " * 10)
